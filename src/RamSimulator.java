7e4f968cab5d2c392b80c2dfd64d5437eec2da52
Isaac Aiman <alu0100841565@ull.edu.es>
2017-02-18 14:57:29 +0000

    Detectados los comentarios y las etiquetas

--- src/RamSimulator.java
+++ src/RamSimulator.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * 
  * 
@@ -8,8 +12,9 @@ public class RamSimulator {
 
 	public static void main(String[] args) {
 		
-		String inFilename = args[0];
-		String outFilename = args[1];
+		String ramProgram = args[0];
+		String inFilename = args[1];
+		String outFilename = args[2];
 		
 		
 		
@@ -18,11 +23,34 @@ public class RamSimulator {
 		
 		tapeOut.setTape(tapeIn.getTape()); 
 		tapeOut.write(outFilename);
+		
+		readProgram (ramProgram);
 
 	}
 	
-	public void readProgram(String filename){
+	public static void readProgram(String filename){
 		
+		try {
+			Scanner scanner = new Scanner(new File(filename));
+			while (scanner.hasNext()){
+				//int i = scanner.nextInt();
+				String next = scanner.next();
+				if (next.equals("#")){
+					scanner.nextLine();
+				}
+				else{
+					System.out.println(next);
+					if (next.endsWith(":")){
+						System.out.println("ETIQUETAAA");
+					}
+				}
+			}
+			scanner.close();
+			
+		} 
+		catch (FileNotFoundException e) {
+		
+		}
 		
 	}
 
